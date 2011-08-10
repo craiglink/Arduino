a89d54b474cc1e6fa699929f011f073101228fe5
Craig Link <craig@moonrock.com>
2011-08-10 13:22:11 -0700

    interrupt safe circular buffer

--- libraries/CircularBuffer/CircularBuffer.h
+++ libraries/CircularBuffer/CircularBuffer.h
@@ -1,30 +1,26 @@
 #ifndef CIRCULARBUFFER_h
 #define CIRCULARBUFFER_h
 
-#include <util/atomic.h>
-
 #ifdef CIRCULARBUFFER_EMPTY_CHECK
-#define CIRCULARBUFFER_NOT_EMPTY !empty()
+#define CIRCULARBUFFER_NOT_EMPTY if(!empty())
 #else
-#define CIRCULARBUFFER_NOT_EMPTY true
+#define CIRCULARBUFFER_NOT_EMPTY 
 #endif
 
 template <class T, class SizeType, uint16_t Size>
 class CircularBuffer {
 public:
   CircularBuffer() :
-    _back(0), _front(0),  _count(0) {}
+    _back(0), _front(0) {}
   ~CircularBuffer() {}
 
   bool push(const T& value) {
-    if ( _count < Size )
+
+    SizeType back = (_back+1)%Size;
+    if ( back != _front   )
     {
         _buf[_back] = value;
-        _back = (_back+1)%Size;
-        ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
-        {
-            _count++;
-        }
+        _back = back;
         return true;
     }
     else
@@ -33,26 +29,22 @@ public:
     }
   }
 
-  inline void pop() {
-    if ( CIRCULARBUFFER_NOT_EMPTY )
+  void pop() {
+    CIRCULARBUFFER_NOT_EMPTY 
     {
         _front = (_front+1)%Size;
-        ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
-        {
-            _count--;
-        }
     }
   }
 
   inline const T& front() {
-    if ( CIRCULARBUFFER_NOT_EMPTY )
+    CIRCULARBUFFER_NOT_EMPTY 
     {
         return _buf[_front];
     }
   }
 
   inline const T& back() {
-    if ( CIRCULARBUFFER_NOT_EMPTY )
+    CIRCULARBUFFER_NOT_EMPTY 
     {
         return _buf[_back];
     }
@@ -60,19 +52,15 @@ public:
 
 
   inline volatile bool empty() const {
-    return _count == 0;
+    return _front == _back;
   }
 
-  inline volatile SizeType size() const {
-    return _count;
-  }
 
 private:
   T _buf[Size];
   SizeType _back;
   SizeType _front;
 
-  volatile SizeType _count;
 };
 
 #endif
